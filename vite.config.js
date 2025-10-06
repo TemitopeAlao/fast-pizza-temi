f84b443c56dad28fb26b8bb6405e9f9035eb3d94
TemitopeAlao <alaotemitope36@gmail.com>
2025-10-06 14:23:02 +0100

    chore: update Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    eslint({
-      include: ["src/**/*.js", "src/**/*.jsx"], // only lint source files
-      failOnWarning: false, // don't stop build on warnings
-      failOnError: false,   // don't stop build on errors
-    }),
-  ],
+  plugins: [react()],
 });
+
